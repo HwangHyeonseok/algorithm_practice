c4950289e22d79d8a2e4e75fde4d6209c6973940
HwangHyeonseok <hhs0995@naver.com>
2023-02-23 23:04:06 +0900

    DP (동적 계획법) 활용 문제 풀이

---
+++ DP(동적 계획법)/10826_피보나치 수 4/10826_피보나치 수 4/10826_피보나치 수 4.cpp
@@ -0,0 +1,32 @@
+﻿// 10826_피보나치 수 4
+// #DP, #큰 수 처리(string)
+// 피보나치 수열의 시간 복잡도는 O(N^2) 이다. 즉, n의 범위가 25보다 클 경우 시간 초과가 날 수도 있다.
+// 현재 문제에서는 n의 범위가 25보다 크므로 DP을 사용해야 한다.
+
+// 실수했던 점 : n이 커질 경우 기하급수적으로 피보나치 값이 커지기 때문에 long long 자료형을 사용해야 안전하다고 생각했으나,
+// long long 자료형 범위도 초과하여 string으로 긴 수에 대한 문자열을 처리해야 했다. (10757번 참고)
+
+#include <iostream>
+#define MAX_N 10001 // N의 최댓값은 10000
+using namespace std;
+
+// dp 사용
+long long int save[MAX_N] = { 0 }; // f(n)값을 저장하기 위한 배열 | 전역 범위인 경우 자동으로 0으로 초기화
+
+long long int fibo(long long int n) {
+	if (n == 0) return 0;
+	if (n == 1 || n == 2) return 1;
+	if (save[n] != 0) // 이미 값이 저장 되어 있는 경우
+		return save[n];
+
+	return save[n] = fibo(n - 1) + fibo(n - 2); // save[n]에 fibo(n-1) + fibo(n-2) 값을 save[n]에 대입하고 그 값을 리턴한다.
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	long long int n;
+	cin >> n;
+	cout << fibo(n);
+
+	return 0;
+}
---
+++ DP(동적 계획법)/24416_알고리즘 수업 - 피보나치 수 1/24416_알고리즘 수업 - 피보나치 수 1/24416_알고리즘 수업 - 피보나치 수 1.cpp
@@ -0,0 +1,55 @@
+﻿// 24416_알고리즘 수업 
+// #재귀vsDP
+// 문제에서 n번째 피보나치 수열을 구해야 하는데, 그 n의 범위가 25를 넘어가는 경우에는 O(N^2)으로 해결하는 재귀함수 방법보다, DP 방법을 사용하여야 한다.
+
+#include <iostream>
+
+using namespace std;
+
+int answer_dp = 0; // DP 연산 횟수
+int answer_recursive = 0; // 재귀 함수 연산 횟수
+
+int save[41] = { 0 };
+
+// DP 풀이 - 중복되는 연산을 배열에 저장하여 시간 복잡도를 O(N)으로 낮춰준다.
+int fibo_dp(int n) {
+	if (n == 1) {
+		answer_dp++;
+		return 1;
+	}
+	if (n == 2) {
+		answer_dp++;
+		return 1;
+	}
+	if (save[n] != 0) {
+		answer_dp++;
+		return save[n]; // 이미 나왔던 값(저장되었던 값)이면 그 값을 리턴
+	}
+
+	return save[n] = fibo_dp(n - 1) + fibo_dp(n - 2);
+}
+
+// 재귀 풀이 -  n이 커지면 연산 횟수는 2의 n-1승 만큼 커지기 때문에 시간 복잡도가 O(n^2) 이라 n이 25를 넘어가면 1초 만에 문제를 해결하지 못한다.
+int fibo_recursive(int n) {
+	// 종료 조건
+	if (n == 1) {
+		answer_recursive++;
+		return 1;
+	}
+	if (n == 2) {
+		answer_recursive++;
+		return 1;
+	}
+
+	// 점화식
+	return fibo_recursive(n - 1) + fibo_recursive(n - 2);
+}
+
+
+int main(void) {
+	int n;
+	cin >> n;
+	fibo_dp(n);
+	fibo_recursive(n);
+	cout << answer_recursive << " " << answer_dp-1;
+}
